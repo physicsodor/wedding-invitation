d37bff5c0620866123e970b5730a46145093ea24
physicsodor <physicsodor@naver.com>
2025-03-21 21:48:44 +0900

    invitation comment

--- src/App.js
+++ src/App.js
@@ -184,9 +184,9 @@ function App() {
       <div className="invit-box">
         <h2>모시는 글</h2>
         <div className="invit-letter">
-          <p>두 사람이 사랑으로 한 가정을 이루게 되었습니다.</p>
-          <p>평생 서로를 귀히 여기고 베풀며 살겠습니다.</p>
-          <p>귀한 발걸음 하시어 축하해주시면 감사하겠습니다.</p>
+          <p>두 사람이 한 가정을 이루고자 합니다.</p>
+          <p>평생 서로 사랑하고 베풀며 살겠습니다.</p>
+          <p>귀한 발걸음으로 축하해주시면 감사하겠습니다.</p>
         </div>
         <div className="invit-name">
           <div>신완기 &middot; 김명재</div>
